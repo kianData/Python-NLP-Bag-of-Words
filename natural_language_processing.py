becf0ac318295a96fdb4c02401b5f728232f7bf1
Kianoosh Keshavarzian <kshvzn@gmail.com>
2021-04-15 18:23:21 +1000

    Add files via upload

---
+++ natural_language_processing.py
@@ -0,0 +1,71 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Evaluating Performance
+tn = cm[0, 0]
+tp = cm[1, 1]
+fp = cm[0, 1]
+fn = cm[1, 0]
+
+accuracy = (tp + tn) / (tp + tn + fp + fn)
+precision = tp / (tp + fp)
+recall = tp / (tp + fn)
+f1Score = 2 * precision * recall / (precision + recall)
+
+performance = [['' for i in range(4)], [0 for j in range(4)]]
+performance[0][0] = 'accuracy'
+performance[0][1] = 'precision'
+performance[0][2] = 'recall'
+performance[0][3] = 'f1Score'
+
+performance[1][0] = accuracy
+performance[1][1] = precision
+performance[1][2] = recall
+performance[1][3] = f1Score
+
+del tp, tn, fp, fn, accuracy, precision, recall, f1Score
